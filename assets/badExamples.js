2df5024a1e7258b1a142b303fb31210636729034
Anya Ji <aj592@cornell.edu>
2021-06-18 22:54:53 -0400

    example

--- assets/badExamples.js
+++ assets/badExamples.js
@@ -48,26 +48,25 @@ export const badExamples = [
       3: "head with hair bun",
       4: "head with hair bun",
       5: "left arm",
-      6: "body",
-      7: "body",
+      6: "dress",
+      7: "dress",
     },
     explanation:
       "The red part could have been split into two separate parts (head, hair bun).",
   },
 
-  // {
-  //   file: "",
-  //   whole: "",
-  //   piece: {
-  //     1: "",
-  //     2: "",
-  //     3: "",
-  //     4: "",
-  //     5: "",
-  //     6: "",
-  //     7: "",
-  //   },
-
-  //   explanation: "",
-  // },
+  {
+    whole: "woman in dress",
+    piece: {
+      1: "woman's head",
+      2: "woman's right arm",
+      3: "woman's hair bun",
+      4: "woman's hair bun",
+      5: "woman's left arm",
+      6: "woman's dress",
+      7: "woman's dress",
+    },
+    explanation:
+      "Could have left out woman's to avoid redundancy.",
+  },
 ];
