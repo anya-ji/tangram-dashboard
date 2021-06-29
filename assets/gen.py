f03456cad251507e2c530d0d5a28cfed18375f73
Anya Ji <aj592@cornell.edu>
2021-06-29 12:06:08 -0400

    json 1006

--- assets/gen.py
+++ assets/gen.py
@@ -3,20 +3,22 @@ from os.path import isfile, join
 import re
 import json
 
-mypath = "./"
+mypath = "/Users/anyaji/Desktop/tangram-online/public/assets"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 pattern = r'points="(.*?)"'
 pattern_viewBox = r'viewBox="(.*?)"'
 
 rs = {}
+i=1
 for file in onlyfiles:
-  if file!="gen.py" and file!="tangrams.json":
-    f = open(file,'r').read().replace("\n"," ")
+  if file.startswith('page') or file == 'a.svg':
+    f = open(join(mypath, file),'r').read().replace("\n"," ")
     points = re.findall(pattern, f)
     viewBox = re.findall(pattern_viewBox, f)
     rs[file] = {'points': points, 'viewBox': viewBox[0]}
-# print(rs)
+    print(i, ': ', file)
+    i+=1
 
 with open('tangrams.json', 'w') as fp:
     json.dump(rs, fp)
