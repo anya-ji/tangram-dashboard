d97591e8a113e5a36e556afc0d5d8de3a2ef4484
Anya Ji <aj592@cornell.edu>
2021-06-29 12:14:50 -0400

    head

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,10 @@ import Button from "@material-ui/core/Button";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import IconButton from "@material-ui/core/IconButton";
-// import InfoIcon from "@material-ui/icons/Info";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import * as FB from "./api/firebase";
 import { useState, useEffect } from "react";
+import Head from "next/head";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -56,6 +53,10 @@ export default function Home() {
   }
   return (
     <div className={classes.root}>
+      <Head>
+        <title>Tangrams Dashboard</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
