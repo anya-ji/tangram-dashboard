88e26a38ec1da2a8f5106632ba594abbca6e49e8
Anya Ji <aj592@cornell.edu>
2022-03-15 12:37:58 +0100

    try to fix styling lost on load again

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import "../styles/globals.css";
+import React from "react";
+import PropTypes from "prop-types";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import HeadTitle from "../components/headTitle";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <>
+    <React.Fragment>
       <HeadTitle />
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </React.Fragment>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
