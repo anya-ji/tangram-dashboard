0ebd851253fb22f9a3740a7a5dcc61f7debc96e8
Anya Ji <aj592@cornell.edu>
2022-03-15 12:41:19 +0100

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,15 @@
+
+   
 import "../styles/globals.css";
-import React from "react";
-import PropTypes from "prop-types";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import HeadTitle from "../components/headTitle";
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
+function MyApp({ Component, pageProps }) {
   return (
-    <React.Fragment>
+    <>
       <HeadTitle />
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
       <Component {...pageProps} />
-    </React.Fragment>
+    </>
   );
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
